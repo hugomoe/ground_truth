a2310a3e2481eeb09b4bbd6daac629be184d401f
hugomoe <hugo.moeneclaey@free.fr>
2015-09-19 23:16:51 +0200

    Update ground_truth.h

--- ground_truth.h
+++ ground_truth.h
@@ -24,7 +24,7 @@ int apply_homo_ground_truth(float *img,float *img_f,int w,int h,int w_f,int h_f,
 	
 	
 	
-	/*for(int l=0;l<3;l++){
+	for(int l=0;l<3;l++){
 	for(int i=0;i<w;i++){
 		for(int j=0;j<h;j++){
 			for(int u=0;u<ZOOM;u++){
@@ -40,11 +40,11 @@ int apply_homo_ground_truth(float *img,float *img_f,int w,int h,int w_f,int h_f,
 			}
 		}
 	}
-	}*/
+	}
 	
 	int w_zoom = ZOOM*w; int h_zoom = ZOOM*h;
 	
-	zoom(img,w,h,3,w_zoom,h_zoom,img_aux);
+	//zoom(img,w,h,3,w_zoom,h_zoom,img_aux);
 	
 	
 	
